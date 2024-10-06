697b5ae6443a980242cc2e6ddd785d8b736fccc5
hayat <122299515+hayutaa@users.noreply.github.com>
2024-10-06 23:20:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
-const app = Vue.createApp({
-    data(){
-      return{
-        newTitle: "",
-        newContent:"",
-        notes: JSON.parse(localStorage.getItem("notes")) || []
-      };
-    },
-  
-    methods: {
-  
-      addNote(){
-        if (this.newTitle.trim() === "" || this.newContent.trim() === ""){
-          alert("Enter a Note");
-          return;
-        }
-  
-        this.notes.push({
-          title: this.newTitle,
-          content: this.newContent
-        });
-  
-        localStorage.setItem("notes", JSON.stringify(this.notes));
-  
-        this.newTitle="";
-        this.newContent = "";
-      },
-  
-      deleteNote(index) {
-        this.notes.splice(index, 1);
-        localStorage.setItem("notes", JSON.stringify(this.notes));
+const {createApp} = Vue;
+
+  const config = {
+  data(){
+    return{
+      newTitle: "",
+      newContent:"",
+      notes: JSON.parse(localStorage.getItem("notes")) || []
+    };
+  },
+
+  methods: {
+
+    addNote(){
+      if (this.newTitle.trim() === "" || this.newContent.trim() === ""){
+        alert("Enter a Note");
+        return;
       }
+
+      this.notes.push({
+        title: this.newTitle,
+        content: this.newContent
+      });
+
+      localStorage.setItem("notes", JSON.stringify(this.notes));
+
+      this.newTitle="";
+      this.newContent = "";
     },
-    mounted(){
-      if (localStorage.getItem("notes")){
-        this.notes = JSON.parse(localStorage.getItem("notes"));
-      }
+
+    deleteNote(index) {
+      this.notes.splice(index, 1);
+      localStorage.setItem("notes", JSON.stringify(this.notes));
     }
-  });
-  
-  app.mount("#app");
+  },
+  mounted(){
+    if (localStorage.getItem("notes")){
+      this.notes = JSON.parse(localStorage.getItem("notes"));
+    }
+  }
+  };
+
+const app = createApp(config);
+app.mount("#app");
   
